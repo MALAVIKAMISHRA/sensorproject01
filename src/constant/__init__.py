305645c4b71079e8314f1d9115de011e31bdefb6
MALAVIKAMISHRA <malavikamishra89@gmail.com>
2024-12-03 19:04:38 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer_fault"
+MANGO_DATABASE_NAME="pwskills"
+MANGO_COLLECTION_NAME="waferfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL=""
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_TYPE=".pkl"
